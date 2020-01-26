95505d1208219c56f16207ca5d128096790e158f
Marvin Laucher <mlaucher@zipcar.com>
2020-01-26 16:19:23 +0000

    add  comment with edit and delete logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_155726) do
+ActiveRecord::Schema.define(version: 2020_01_26_152137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -28,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_01_25_155726) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
-    t.integer "blog_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -36,18 +28,6 @@ ActiveRecord::Schema.define(version: 2020_01_25_155726) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
